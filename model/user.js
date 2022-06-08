e9fc6b7aba8ca8420668c20243da99fe1ccb1113
mahmoudnouridev <107126697+mahmoudnouridev@users.noreply.github.com>
2022-06-08 18:52:33 +0430

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,1 +1,34 @@
 
+const db = require('../db/user');
+const pass = require('../general/password');
+
+module.exports = {
+    
+    create: async function(email, name, password, address){
+        
+        let password_hash = await this.getPasswordHash(password);
+        return await db.create(email, name, password_hash, address);
+        
+    },
+    
+    
+    
+    edit: async function(id, email, name, address){
+        
+        return await db.edit(id, email, name, address);
+        
+    },
+    
+    
+    remove: async function(id){
+        
+        return await db.remove(id);
+        
+    },
+    
+    getPasswordHash: async function(password){
+        
+        return await pass.hash(password);
+    }
+    
+}
