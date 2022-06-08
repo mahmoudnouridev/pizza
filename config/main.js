576e1049aeada3254b6f24ba44fc99d0f1c2bbb6
mahmoudnouridev <107126697+mahmoudnouridev@users.noreply.github.com>
2022-06-08 18:48:40 +0430

    Add files via upload

--- config/main.js
+++ config/main.js
@@ -1,1 +1,20 @@
+const config = {
+    
+    db: {
+        
+        mongodb:{
+        
+            username: '',
+            password: '',
+            host: '127.0.0.1',
+            port: '27017',
+            options: ''
+            
+        }                
+    }   
+    
+    
+}
 
+
+module.exports = config;
