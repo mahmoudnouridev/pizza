a807425a286f6b9c73e4dbab15effd5b432540b0
mahmoudnouridev <107126697+mahmoudnouridev@users.noreply.github.com>
2022-06-09 23:21:34 +0430

    Create user.js

---
+++ controller/user.js
@@ -0,0 +1,34 @@
+const BaseController = require('./baseController');
+const userModel = require('../model/user');
+
+class user extends BaseController{
+      
+    async create(req, res){
+        
+        let password_rules = {'minLen': 8};
+        let expected = {'email': {'email': true, 'maxLen': 30}, 'fname': {'minLen': 3, 'maxLen': 20}, 'address':{'maxLen': 30}, 'password': password_rules, 'password_confirm': password_rules};
+        let required = ['email', 'fname', 'address', 'password', 'password_confirm'];
+        this.validator.checkStrings(req, expected, required);
+        
+        if(this.hasError())
+        {
+            res.send({'error': this.errors});
+            return;
+        }
+        if(this.clean.password !== this.clean.password_confirm)
+        {
+            this.setError('password_confirm', 'match', 'password');
+            res.send({'error': this.errors});
+            return;
+        }
+        
+        let user = new userModel();
+        await user.create(this.clean.email, this.clean.fname, this.clean.password, this.clean.address);
+        
+        res.send({'action': true});            
+    }
+    
+}
+
+
+module.exports = User;
