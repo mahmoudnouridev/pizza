f8491939a1e5081545e9fb9d4130252359152254
mahmoudnouridev <107126697+mahmoudnouridev@users.noreply.github.com>
2022-06-09 23:24:02 +0430

    Create user.js

---
+++ db/user.js
@@ -0,0 +1,45 @@
+const db = require('db');
+
+class user extends db{
+    
+    
+    async create(email, name, password, address){
+        
+        let doc = {'email': email, 'name': name, 'password_hash': password, 'address': address};
+        return await db.con.users.insertOne( doc );
+        
+    }
+    
+    async edit(id, email, name, address){
+        
+        return await db.con.users.updateOne({'_id': id},{
+           
+            $set{
+                
+                'email': email,
+                'name': name,
+                'address': address
+                
+            }
+            
+        });
+        
+    }
+    
+    async remove(id){
+        
+        return await db.con.deleteOne({'_id': id});
+        
+    }
+    
+    async getLoginInfo(email){
+        
+        let projection = {'password_hash': 1};
+        return await db.con.users.findOne({'email': email}, {$projection: projection});
+        
+    }
+    
+    
+    
+    
+}
