b22c7b18745839083b242e40f2d1d64c004edd1d
mahmoudnouridev <107126697+mahmoudnouridev@users.noreply.github.com>
2022-06-10 21:44:22 +0430

    Update food.js

--- db/food.js
+++ db/food.js
@@ -1,4 +1,4 @@
-const db = require('db');
+const db = require('./db');
 
 class food extends db{
     
@@ -11,7 +11,7 @@ class food extends db{
     
     async getAll(){
         
-        return await db.con.menu.find({});
+        return await db.con.menu.find({}).toArray();
         
     }
     
