28bdc341c791263594aa3ed155a2403231dddccc
mahmoudnouridev <107126697+mahmoudnouridev@users.noreply.github.com>
2022-06-12 14:37:50 +0430

    Update routes.js

--- view/routes.js
+++ view/routes.js
@@ -11,41 +11,20 @@ app.get('/login', (req, res)=>{
 });
 
 
-app.post('/login', (req, res)=>{
-    
-    // login user here
-    sess.login(req, res);
-    
-});
-
-app.get('/protected/logout', guard.auth, (req, res)=>{
-   
-   sess.logout(req, res);
-    
-});
-
-app.get('/protected/menu', guard.auth, (req, res)=>{
-    
-    // send to user all menu items
-    food.getAll(req, res);
-    
-});
-
-app.post('/admin/menu/add', guard.only_admin, (req, res)=>{
-    
-    food.create(req, res);
-    
-});
-
 app.get('/register', (req, res)=>{
     
     // render register page
 });
+    
 
-app.post('/register', (req, res)=>{
+app.post('/login', sess.login);
+
+app.get('/protected/logout', guard.auth, sess.logout);
     
-   user.create(req, res);
-    
-});
+app.get('/protected/menu', guard.auth, food.getAll);
+
+app.post('/admin/menu/add', guard.only_admin, food.create);
+
+app.post('/register', user.create);
 
 }
